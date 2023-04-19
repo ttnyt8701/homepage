394f088e54982204285d917a7167e8037305c64f
ttnyt <tyuuta519@gmail.com>
2023-04-20 04:38:59 +0900

    update

---
+++ pages/[id].tsx
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+
+const Product = () => {
+    const router = useRouter();
+    const {id} = router.query;
+    return (
+        <div>
+            {id}
+        </div>
+    );
+}
+
+export default Product;
